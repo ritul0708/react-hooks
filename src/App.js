12b6ccf0778ebad3744ea4dce0c36dcaaa5f2337
Ritul Jain <rituljain0708@gmail.com>
2023-03-01 19:39:06 +0530

    counter using useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from "react"; 
-import ComponentA from "./components/context/ComponentA";
+import CounterOne from "./components/reducer/CounterOne";
 
-export const UserContext = React.createContext();
-export const ChannelContext = React.createContext();
-
-function App() {
+const App = () => {
   return (
     <div>
-      <UserContext.Provider value={'ritul'}>
-        <ChannelContext.Provider value={'react learning'}>
-          <ComponentA />
-        </ChannelContext.Provider>
-      </UserContext.Provider>
+      <CounterOne />
     </div>
-  );
+  )
 }
+
+
+
+// --------- Use This One for Context Examples ------- //
+
+// export const UserContext = React.createContext();
+// export const ChannelContext = React.createContext();
+
+// function App() {
+//   return (
+//     <div>
+//       <UserContext.Provider value={'ritul'}>
+//         <ChannelContext.Provider value={'react learning'}>
+//           <ComponentA />
+//         </ChannelContext.Provider>
+//       </UserContext.Provider>
+//     </div>
+//   );
+// }
+
+// --------- Use This One for Context Examples ------- //
 
 export default App;
